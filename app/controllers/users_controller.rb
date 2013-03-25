ae8f5564dbc97189d00d8908050c0cd313bc33a7
Eric Bitzegaio <ericbitzegaio@pham.home>
2013-03-25 19:56:33 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,13 @@ class UsersController < ApplicationController
   before_filter :admin_user,      only: :destroy
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    if !User.find(params[:id]).admin?
+      User.find(params[:id]).destroy 
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    else
+      redirect_to root_path
+    end
   end
 
   def index
@@ -16,13 +20,25 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
+
+    if signed_in?
+      redirect_to root_url
+    end
+
     @user = User.new
+  
   end
 
   def create
+
+    if signed_in?
+      redirect_to root_url
+    end
+
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -48,13 +64,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,6 +6,18 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "as an admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+
+      before { sign_in admin }
+
+      describe "admin submitting a DELETE request to delete himself" do
+        before { delete user_path(admin) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
+
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
@@ -21,20 +33,42 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
+          sign_in user
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
         end
-      
 
         describe "after signing in" do
           
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              sign_in user
+            end
+            
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name)
+            end
+          end
+
         end
       end
 
@@ -108,6 +142,13 @@ describe "Authentication" do
       it { should have_link('Settings',     href: edit_user_path(user)) }
       it { should have_link('Sign out',     href: signout_path) }
       it { should_not have_link('Sign in',  href: signin_path) }
+    end
+
+    describe "not signed in" do
+      it { should_not have_link('Users') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out') }
     end
   end
 end
